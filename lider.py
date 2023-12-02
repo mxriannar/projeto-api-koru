4c9b7776443c91c63c3ef02537c5c19c4bc0301e
Joel <joelvieirasouza@gmail.com>
2023-12-01 23:42:12 -0300

    Update lider.py

--- lider.py
+++ lider.py
@@ -1,27 +1,29 @@
 import sqlite3
 
 class Lider:
-    def __init__(self, nome:str, departamento:str, id=None):
+    def __init__(self, nome:str, departamento:str, email:str, id=None):
         self.id = id
         self.nome = nome
         self.departamento = departamento
+        self.email = email
 
     def to_dict(self):
         return {
             "id": self.id,
             "nome": self.nome,
-            "departamento": self.departamento
+            "departamento": self.departamento,
+            "email": self.email
         }
     
     def save(self, db_connection:sqlite3.Connection):
         if self.id is None:
-            query = "INSERT INTO lideres (nome_lider, departamento_lider) VALUES (?, ?)"
+            query = "INSERT INTO lideres (nome_lider, departamento_lider, email_lider) VALUES (?, ?, ?)"
             cursor = db_connection.cursor()
-            cursor.execute(query, (self.nome, self.departamento))
+            cursor.execute(query, (self.nome, self.departamento, self.email))
             self.id = cursor.lastrowid
         else:
-            query = "UPDATE lideres SET nome_lider = ?, departamento_lider = ? WHERE id_lider = ?"
-            db_connection.execute(query, (self.nome, self.departamento, self.id))
+            query = "UPDATE lideres SET nome_lider = ?, departamento_lider = ?, email_lider = ? WHERE id_lider = ?"
+            db_connection.execute(query, (self.nome, self.departamento, self.email, self.id))
         db_connection.commit()
         db_connection.close()
 
@@ -37,7 +39,7 @@ class Lider:
         cursor = db.cursor()
         result = cursor.execute(query, (id, )).fetchone()
         if result:
-            return Lider(id = result[0], nome = result[1], departamento = result[2])
+            return Lider(id = result[0], nome = result[1], departamento = result[2], email = result[3])
         else:
             return None
         
@@ -48,6 +50,6 @@ class Lider:
         results = cursor.execute(query).fetchall()
         lideres = []
         for result in results:
-            lideres.append(Lider(id = result[0], nome = result[1], departamento = result[2]).to_dict())
+            lideres.append(Lider(id = result[0], nome = result[1], departamento = result[2], email = result[3]).to_dict())
         return lideres
     
