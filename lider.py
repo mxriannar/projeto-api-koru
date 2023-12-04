ce6f4a5bb21275dfc64910284e8a6a92e5a88cc6
Joel <joelvieirasouza@gmail.com>
2023-12-03 21:56:02 -0300

    Update lider.py

--- lider.py
+++ lider.py
@@ -1,37 +1,41 @@
 import sqlite3
 
 class Lider:
-    def __init__(self, nome:str, departamento:str, email:str, id=None):
+    def __init__(self, nome:str, departamento:str, email:str, password:str, ativo:int, id=None):
         self.id = id
         self.nome = nome
         self.departamento = departamento
         self.email = email
+        self.password = password
+        self.ativo = ativo
 
     def to_dict(self):
         return {
             "id": self.id,
             "nome": self.nome,
             "departamento": self.departamento,
-            "email": self.email
+            "email": self.email,
+            "password": self.password,
+            "ativo": self.ativo
         }
     
     def save(self, db_connection:sqlite3.Connection):
         if self.id is None:
-            query = "INSERT INTO lideres (nome_lider, departamento_lider, email_lider) VALUES (?, ?, ?)"
+            query = "INSERT INTO lideres (nome_lider, departamento_lider, email_lider, password_lider, ativo_lider) VALUES (?, ?, ?, ?, ?)"
             cursor = db_connection.cursor()
-            cursor.execute(query, (self.nome, self.departamento, self.email))
+            cursor.execute(query, (self.nome, self.departamento, self.email, self.password, self.ativo))
             self.id = cursor.lastrowid
         else:
-            query = "UPDATE lideres SET nome_lider = ?, departamento_lider = ?, email_lider = ? WHERE id_lider = ?"
-            db_connection.execute(query, (self.nome, self.departamento, self.email, self.id))
+            query = "UPDATE lideres SET nome_lider = ?, departamento_lider = ?, email_lider = ?, password_lider = ?, ativo_lider = ? WHERE id_lider = ?"
+            db_connection.execute(query, (self.nome, self.departamento, self.email, self.password, self.ativo, self.id))
         db_connection.commit()
         db_connection.close()
 
     def delete(self, db:sqlite3.Connection):
-        query = "DELETE FROM lideres WHERE id_lider = ?"
-        cursor = db.cursor()
-        cursor.execute(query, (self.id, ))
+        query = "UPDATE lideres SET ativo = ? WHERE id_lider = ?"
+        db.execute(query, (self.ativo, self.id))
         db.commit()
+        db.close()
         
     @staticmethod
     def get_by_id(id:int, db:sqlite3.Connection):
@@ -39,7 +43,7 @@ class Lider:
         cursor = db.cursor()
         result = cursor.execute(query, (id, )).fetchone()
         if result:
-            return Lider(id = result[0], nome = result[1], departamento = result[2], email = result[3])
+            return Lider(id = result[0], nome = result[1], departamento = result[2], email = result[3], password = result[4], ativo = result[5])
         else:
             return None
         
@@ -50,6 +54,6 @@ class Lider:
         results = cursor.execute(query).fetchall()
         lideres = []
         for result in results:
-            lideres.append(Lider(id = result[0], nome = result[1], departamento = result[2], email = result[3]).to_dict())
+            lideres.append(Lider(id = result[0], nome = result[1], departamento = result[2], email = result[3], password = result[4], ativo = result[5]).to_dict())
         return lideres
     
