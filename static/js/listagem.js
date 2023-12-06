704f48d4c9f6c3e0f86f9dfb9838954d6ef4c97f
Marianna Rodrigues <mxriannar@gmail.com>
2023-12-06 18:23:44 -0300

    Criando o filtro de pesquisa para as tabelas.

--- static/js/listagem.js
+++ static/js/listagem.js
@@ -1,12 +1,48 @@
 let popup = document.getElementById('popup');
 let blur = document.getElementById('blur');
 
-function openPopup(){
+function openPopup() {
     popup.classList.add('open-popup');
     blur.classList.add('active');
 }
 
-function closePopup(){
+function closePopup() {
     popup.classList.remove('open-popup');
     blur.classList.remove('active');
+}
+
+// Filtro de pesquisa
+function filterTable() {
+    let input = document.getElementById("search"),
+        filter = input.value.toLowerCase(),
+        table = document.getElementById("myTable"),
+        rows = table.getElementsByTagName("tr");
+
+    if (filter != "") {
+        for (i = 1; i < rows.length; i++) {
+            let cells = rows[i].getElementsByTagName("td"),
+                j,
+                rowContainsFilter = false;
+
+            for (j = 0; j < cells.length; j++) {
+                if (cells[j]) {
+                    let cellValue = cells[j].innerHTML.toLowerCase();
+
+                    if (cellValue.includes(filter) && !cellValue.includes("<button")) {
+                        rowContainsFilter = true;
+                        break
+                    }
+                }
+            }
+            if (!rowContainsFilter) {
+                rows[i].style.display = "none";
+            } else {
+                rows[i].style.display = "";
+            }
+        }
+    } else {
+        for (let index = 0; index < rows.length; index++) {
+            rows[index].style.display = "";
+        }
+    }
 }
