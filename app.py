927af042e545edb241ef5050621543439d138865
Marianna Rodrigues <mxriannar@gmail.com>
2023-12-06 21:12:18 -0300

    Criando lógica para ordenação crescente e decrescente das colunas.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db = DBConnector("feedback.db")
 
 @app.route("/")
 def home():
-    return render_template("lider.html")
+    return render_template("index.html")
 
 # Retornar todos os líderes
 @app.route("/lideres", methods=["GET"])
--- static/js/listagem.js
+++ static/js/listagem.js
@@ -1,3 +1,4 @@
+// Popup botão cancelar
 let popup = document.getElementById('popup');
 let blur = document.getElementById('blur');
 
@@ -15,7 +16,7 @@ function closePopup() {
 function filterTable() {
     let input = document.getElementById("search"),
         filter = input.value.toLowerCase(),
-        table = document.getElementById("myTable"),
+        table = document.getElementById("table-sortable"),
         rows = table.getElementsByTagName("tr");
 
     if (filter != "") {
@@ -46,3 +47,50 @@ function filterTable() {
         }
     }
 }
+
+// / Ordenar crescente e decrescente
+/**
+ * Sorts a HTML table
+ * 
+ * @param {HTMLTableElement} table The table to sort
+ * @param {number} column The index of the column to sort
+ * @param {boolean} asc Determines if the sorting will be in ascending order
+ */
+
+function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows_table = Array.from(tBody.querySelectorAll('tr'));
+debugger
+    // Sort each row
+    const sortedRows = rows_table.sort((a, b) => {
+        debugger
+        const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+
+        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    })
+
+    // Remove all existing TRs from the table 
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll('th').forEach(th => th.classList.remove('th-sort-asc', 'th-sort-desc'));
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle('th-sort-asc', asc);
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle('th-sort-desc', !asc);
+}
+
+document.querySelectorAll('.table-sortable th').forEach(headerCell => {
+    headerCell.addEventListener('click', () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains('th-sort-asc');
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    })
+})
